aba1cdb7ac88a76b2a88af5bf8b505656602f361
Alvaroxx1 <alvaroroque.jaragaleano.uptp@gmail.com>
2023-09-18 11:13:02 +0800

    Adding some java files

--- src/Blackjack/Game.java
+++ src/Blackjack/Game.java
@@ -1,9 +1,20 @@
 package Blackjack;
-
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 public class Game {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java");
+		
+		ArrayList<Integer> IDs = new ArrayList<Integer>();
+		IDs.add(1234);
+		IDs.add(34234);
+		IDs.add(23552);
+		Collections.sort(IDs);
+		IDs.remove(2);
+		System.out.println(IDs);
+		
+		
 
 	}
 
