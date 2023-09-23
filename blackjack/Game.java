17e03854eb59bf641c935cb7ae9feca9af783ecb
Jaime <mjackerjaime@gmail.com>
2023-09-23 12:12:27 +0800

    Revert "Deleting duplicated files"
    This reverts commit 6b9b1575509b60d551e6431a3a5e94cf1a36be14.

---
+++ blackjack/Game.java
@@ -0,0 +1,29 @@
+package Blackjack;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class Game {
+	
+	public static void main(String[] args) {
+		Dictionary<String, Integer> dict = new Hashtable<>();
+		dict.put(null, null);
+
+		
+		
+
+	}
+	// Functions
+	static void hit() {;
+	
+	}
+	static void stand() {
+		
+	};
+	static void bet() {
+	
+	}
+
+}
