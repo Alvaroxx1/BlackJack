3d3589dce6425bc977d288e216ea5bf4f1b39383
Alvaroxx1 <alvaroroque.jaragaleano.uptp@gmail.com>
2023-09-23 12:07:09 +0800

    Creating the card object

--- src/Blackjack/Blackjack/Main.java
+++ src/Blackjack/Blackjack/Main.java
@@ -6,6 +6,28 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main (String[] args) {
+		
+		Card mycard = new Card("4","h");
+
+		System.out.println(mycard.toString());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
 		// Test creando persons
 		Player p1 = new Player("jaime", 10);
 		Player p2 = new Player("Alvaro", 10);
@@ -38,6 +60,7 @@ public class Main {
 		for (Player num : players) { 		      
 	           System.out.println(num); 		
 	      }
+	      */
 		
 		
 	}
--- src/Blackjack/Objects/Deck.java
+++ src/Blackjack/Objects/Deck.java
@@ -1,5 +1,27 @@
 package Objects;
 
-public class Deck {
+import java.util.ArrayList;
 
+public class Deck {
+	private String[] index = {"2","3","4","5","6","7",
+			"8","9","T","J","Q","K","A"};
+	private String[] suits = {"d","c","h","s"};
+	
+	private ArrayList<Card> cards;
+	
+	public Deck() {
+		
+		Card mycard = new Card("4","h");
+		System.out.println(mycard.toString());
+		
+//		for(int n = 0;n<=3;n++) {
+//			for(int i = 2; i<10;i++) {
+//				
+//				
+//			}
+//		}
+		
+	}
+	
+	
 }
